8849bae51e38d833c3c90969e72193e611d5fe07
anetigor <114143140+anetigor@users.noreply.github.com>
2024-04-18 13:05:28 +0200

    Update task-07.js

--- pliki-startowe/js/task-07.js
+++ pliki-startowe/js/task-07.js
@@ -3,14 +3,14 @@
     const fontSizeControl = document.getElementById('font-size-control');
     const textSpan = document.getElementById('text');
 
-    // Ustawienie początkowego rozmiaru czcionki z wartości początkowej suwaka
+   
     textSpan.style.fontSize = fontSizeControl.value + 'px';
 
-    // Funkcja, która aktualizuje rozmiar czcionki tekstu, kiedy wartość suwaka się zmienia
+   
     function adjustFontSize() {
       textSpan.style.fontSize = fontSizeControl.value + 'px';
     }
 
-    // Dodanie nasłuchiwacza na zdarzenie 'input'
+    
     fontSizeControl.addEventListener('input', adjustFontSize);
   });
